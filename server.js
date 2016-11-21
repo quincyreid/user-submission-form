a0816d6c6b409b00d98c31eb8a76213cd66aa515
Quincy Reid <qreid51@gmail.com>
2016-11-21 14:56:32 -0500

    Adds view directory, potential form and points to it

--- server.js
+++ server.js
@@ -2,8 +2,16 @@
  * Server
  */
 
+const path = require('path')
 const express = require('express')
+const router = require('./lib/router')
 const app = express()
+
+app.set('views', path.join(__dirname, '/views'))
+app.engine('.html', require('ejs').__express)
+app.set('view engine', 'html')
+
+app.use('/', router)
 
 if (!module.parent) {
   app.listen(process.env.PORT || 3000)
